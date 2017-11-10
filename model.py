9d65e58be27ac3e5f4c2771bbf6c0399b0bb8725
Tran Minh Tuan <tuantmtb@gmail.com>
2017-11-10 17:43:30 +0700

    update model

--- model.py
+++ model.py
@@ -2,6 +2,8 @@
 
 import codecs, os, io
 import re, math
+from builtins import enumerate
+
 from pyvi.pyvi import ViTokenizer
 from sklearn.externals import joblib
 from sklearn.pipeline import Pipeline
@@ -9,6 +11,7 @@ import numpy as np
 
 folder_train = '/Volumes/DATA/workspace/vnu/spc/git/doit-classification/DataSource/Data_raw/train'
 folder_test = '/Volumes/DATA/workspace/vnu/spc/git/doit-classification/DataSource/Data_raw/test'
+
 
 def get_filepaths(directory):
     file_paths = []  # List which will store all of the full filepaths.
@@ -42,35 +45,25 @@ def load_dataset(folder):
                     dataset['target'].append(position)
             position += 1
 
-    return dataset, dataset['target_names']
+    return dataset
 
 
 def train():
-    train_set, target_names = load_dataset(folder_train)
-    joblib.dump(train_set, 'train_set.pkl')
-    print('loaded train_set')
+    # train_set = load_dataset(folder_train)
+    # joblib.dump(train_set, 'train_set.pkl')
+    # print('loaded train_set')
+    train_set = joblib.load('train_set.pkl')
 
     # Extracting features from text files
     from sklearn.feature_extraction.text import CountVectorizer
-
-    count_vect = CountVectorizer()
-    X_train_counts = count_vect.fit_transform(train_set['data'])
-    joblib.dump(X_train_counts, 'X_train_counts.pkl')
-    print(X_train_counts.shape)
-
     # TF-IDF
     from sklearn.feature_extraction.text import TfidfTransformer
-    tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-    joblib.dump(X_train_tfidf, 'X_train_tfidf.pkl')
-    print('tfidf')
-    print(X_train_tfidf.shape)
 
     # Training Naive Bayes (NB) classifier on training data.
     # from sklearn.naive_bayes import MultinomialNB
     # text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
-
-    print('training')
+    #
+    # print('training')
     # text_clf = text_clf.fit(train_set['data'], train_set['target'])
     # joblib.dump(text_clf, 'text_clf.pkl')
 
@@ -82,19 +75,22 @@ def train():
 
     text_clf_svm = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()),
                              ('clf-svm',
-                              SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42))])
+                              SGDClassifier(loss='modified_huber', penalty='l2', n_jobs=-1, alpha=1e-3, n_iter=5,
+                                            random_state=42))])
 
     text_clf_svm = text_clf_svm.fit(train_set['data'], train_set['target'])
     joblib.dump(text_clf_svm, 'text_clf_svm.pkl')
-    return target_names
+    print('train success')
+
 
 def test():
-    # test_set = load_dataset(folder_test)
-    # joblib.dump(test_set, 'test_set.pkl')
     print('process test')
-    test_set = joblib.load('test_set.pkl')
+    test_set = load_dataset(folder_test)
+    joblib.dump(test_set, 'test_set.pkl')
+    # test_set = joblib.load('test_set.pkl')
     print('loaded test_set')
     text_clf_svm = joblib.load('text_clf_svm.pkl')
+    # text_clf_svm = joblib.load('text_clf.pkl')  # bayes
     predicted_svm = text_clf_svm.predict(test_set['data'])
     print(np.mean(predicted_svm == test_set['target']))
 
@@ -107,19 +103,39 @@ def load_text(doc):
     dataset['data'].append(ViTokenizer.tokenize(content))
     return dataset
 
+
 def predict(predict_set):
     print('process predict')
     text_clf_svm = joblib.load('text_clf_svm.pkl')
     # predicted_svm = text_clf_svm.predict_proba(predict_set['data'])
-    predicted_svm = text_clf_svm.predict(predict_set['data'])
+    predicted_svm = text_clf_svm.predict_proba(predict_set['data'])
     # print(predicted_svm)
     target_names = joblib.load('train_set.pkl')['target_names']
-    index_classification = math.floor(np.mean(predicted_svm))
-    return index_classification, target_names[index_classification]
+
+    # index_classification = math.floor(np.mean(predicted_svm))
+    names_predict_output = []
+    # print(predicted_svm[0][0])
+
+    for index in range(len(predicted_svm[0])):
+        names_predict_output.append({'index': index, 'score': predicted_svm[0][index], 'name': target_names[index]})
+    print(names_predict_output)
+
+
+
 
 def load_file(filePath):
     with io.open(filePath, mode="r", encoding="UTF8") as file:
         return file.read().lower()
 
+# train()
 
+# test()
 
+doc = load_file(
+    '/Volumes/DATA/workspace/vnu/spc/git/doit-classification/DataSource/Data_raw/train/KHTN/01050002682.txt')
+# print(doc)
+predict_set = load_text(doc)
+predict(predict_set)
+# index_classification, target_name = predict(predict_set)
+# print(index_classification)
+# print(target_name)
--- web.py
+++ web.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
 @app.route("/predict", methods=['POST'])
 def predict():
     req = request.json
@@ -22,4 +23,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0',port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
